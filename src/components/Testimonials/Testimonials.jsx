efc7fa08bd6396a636f335ce4db226e31642df37
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-03-14 15:24:31 +0100

    testimonials swiper/carousel added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { data } from '../../assets/data/testimonialData';
 
+// import Swiper core and required modules
+import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 import './testimonials.css';
 
 export const Testimonials = () => {
@@ -8,35 +17,27 @@ export const Testimonials = () => {
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container">
+
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination, Navigation]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
         {data.map(({ avatar, name, review }, index) => {
           return (
-            <article key={index} className="testimonial">
+            <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt="avatar one" />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
-
-{
-  /* <article className="testimonial">
-<div className="client__avatar">
-  <img src={AV1} alt="avatar one" />
-</div>
-<h5 className="client__name">kira riddick</h5>
-<small className="client__review">
-  Lorem ipsum dolor sit amet. Vel eius illo ea quaerat inventore a
-  culpa fugiat non numquam dignissimos in atque mollitia. Vel possimus
-  sapiente aut praesentium molestias ut aspernatur facilis id rerum
-  pariatur id quia dolorem sit veritatis numquam 33 voluptatum
-  provident. Qui culpa numquam et possimus quia aut galisum quis.
-</small>
-</article> */
-}
