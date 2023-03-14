d5d5fa5aa5df1b67f39494d7369558585b17826c
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-03-14 14:01:17 +0100

    portfolio component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CTA } from './CTA';
 import './header.css';
-import ME from '../../asets/me.jpg';
+import ME from '../../assets/me.jpg';
 import { HeaderSocials } from './HeaderSocials';
 
 export const Header = () => {
